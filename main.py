59d9fe7e8ef040e5925b9b62bc93599b9906101b
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-11-07 02:41:42 +0100

    models creation with GRIDCV

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from utils import Utils
 from models import Models
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     utils = Utils()
     models = Models()
-
-    data = utils.load_from_csv('./in/felicidad.csv')
-    X, y = utils.features_target(data, ['score', 'rank', 'country'], ['score'])
-    models.grid_training(X, y)
-    print(data)
+    dataFrame1 = utils.load_from_csv('./in/dataAnimals.csv')
+    dataFrame2 = utils.load_from_csv('./in/dataDataScience.csv')
+    dataFrame3 = utils.load_from_csv('./in/dataEducation.csv')
+    dataFrame4 = utils.load_from_csv('./in/dataFashion.csv')
+    dataFrame5 = utils.load_from_csv('./in/dataPolitics.csv')
+    frames = [dataFrame1, dataFrame2, dataFrame3, dataFrame4, dataFrame5]
+    alldata = pd.concat(frames)
+    alldata_data = alldata.values[:, 2]
+    alldata_tarjet = alldata.values[:, 1]
+    vectorizer = TfidfVectorizer()
+    x_train = vectorizer.fit_transform(alldata_data)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_train, alldata_tarjet, test_size=0.33, random_state=42)
+    # data = utils.load_from_csv('./in/felicidad.csv')
+    # X, y = utils.features_target(data, ['score', 'rank', 'country'], ['score'])
+    #models.grid_training(X, y)
+    models.grid_training(x_train, y_train)
+    print(alldata)
--- models.py
+++ models.py
@@ -4,7 +4,8 @@ import numpy as np
 from sklearn.svm import SVR
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
 from utils import Utils
 
 
@@ -12,8 +13,10 @@ class Models:
 
     def __init__(self):
         self.reg = {
-            'SVR': SVR(),
-            'GRADIENT': GradientBoostingRegressor()
+            # 'SVR': SVR(),
+            # 'GRADIENT': GradientBoostingRegressor(),
+            'NB': MultinomialNB(),
+            'KNN': KNeighborsClassifier()
         }
 
         self.params = {
@@ -25,6 +28,12 @@ class Models:
             'GRADIENT': {
                 'loss': ['ls', 'lad'],
                 'learning_rate': [0.01, 0.05, 0.1]
+            },
+            'NB': {
+                'alpha': [.01]
+            },
+            'KNN': {
+                'n_neighbors': [3, 5, 11, 19]
             }
         }
 
@@ -33,7 +42,8 @@ class Models:
         best_model = None
         for name, reg in self.reg.items():
             grid_reg = GridSearchCV(
-                reg, self.params[name], cv=3).fit(X, y.values.ravel())
+                # reg, self.params[name], cv=3).fit(X, y.values.ravel())
+                reg, self.params[name], cv=3).fit(X, y)
             score = np.abs(grid_reg.best_score_)
 
             if score < best_score:
