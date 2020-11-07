cc9fc6c4ce760cf008ffc719975ad1d6df76c153
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-11-07 23:19:56 +0100

    Neuronal Networks and Decision trees

--- models.py
+++ models.py
@@ -5,7 +5,9 @@ from sklearn.svm import SVR
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn import tree
 from utils import Utils
 
 
@@ -13,42 +15,47 @@ class Models:
 
     def __init__(self):
         self.reg = {
-            # 'SVR': SVR(),
-            # 'GRADIENT': GradientBoostingRegressor(),
             'NB': MultinomialNB(),
-            'KNN': KNeighborsClassifier()
+            'KNN': KNeighborsClassifier(),
+            'Nn': MLPClassifier(),
+            'TreeCl': tree.DecisionTreeClassifier()
         }
 
         self.params = {
-            # 'SVR': {
-            #    'kernel': ['linear', 'poly', 'rbf'],
-            #    'gamma': ['auto', 'scale'],
-            #    'C': [1, 5, 10]
-            # },
-            # 'GRADIENT': {
-            #    'loss': ['ls', 'lad'],
-            #    'learning_rate': [0.01, 0.05, 0.1]
-            # },
             'NB': {
-                'alpha': [.01]
+                'alpha': [.01, .04, .005]
             },
             'KNN': {
                 'n_neighbors': [3, 5, 11, 19]
+            },
+            'Nn': {
+                'activation': ['identity', 'logistic', 'tanh', 'relu'],
+                'solver': ['lbfgs', 'sgd', 'adam'],
+                'hidden_layer_sizes': [
+                    (1,), (2,), (3,), (4,), (12,), (13,), (14,), (15,), (30,), (80,)
+                ]
+            },
+            'TreeCl': {
+                'criterion': ['gini', 'entropy'],
+                'max_depth': [4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 30, 40, 50, 70, 90, 120, 150]
             }
+
         }
 
     def grid_training(self, X, y):
-        best_score = 999
+        best_score = 0.00999
         best_model = None
         for name, reg in self.reg.items():
             grid_reg = GridSearchCV(
-                # reg, self.params[name], cv=3).fit(X, y.values.ravel())
-                reg, self.params[name], cv=3).fit(X, y)
+                reg, self.params[name], cv=3, scoring='accuracy').fit(X, y)
             score = np.abs(grid_reg.best_score_)
-
-            if score < best_score:
+            print("Score: "+str(score))
+            print("Model: "+str(grid_reg.best_estimator_))
+            if score > best_score:
                 best_score = score
                 best_model = grid_reg.best_estimator_
 
+        print("BEST Score::: "+str(best_score))
+        print("BEST Model::: "+str(best_model))
         utils = Utils()
         utils.model_export(best_model, best_score)
--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import joblib
+import numpy as np
 
 
 class Utils:
 
     def load_from_csv(self, path):
         return pd.read_csv(path)
- 
+
     def features_target(self, dataset, drop_cols, y):
         X = dataset.drop(drop_cols, axis=1)
         y = dataset[y]
         return X, y
 
     def model_export(self, clf, score):
-        joblib.dump(clf, './models/'+str(score))
+        print('name: ' + str(clf))
+        score = np.abs(score)
+        joblib.dump(clf, './models/' + str(score))
