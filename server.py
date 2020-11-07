a68db4a76729769993aacf33a60d94e0f085c5c5
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-11-07 18:20:49 +0100

    updated all working

--- server.py
+++ server.py
@@ -34,20 +34,33 @@ def predict():
     print(alldata_one)
     vectorizer = TfidfVectorizer()
     x_train = vectorizer.fit_transform(alldata_data)
-    x_test = vectorizer.transform(['where is Dog'])
+    x_test = vectorizer.transform(['where is Obama'])
     prediction = model.predict(x_test)
     return jsonify({'prediccion': list(prediction)})
 
 
 @app.route('/api', methods=['POST'])
 def api():
+    utils = Utils()
     if request.method == 'POST':
-
         wordsU = request.form['words']
-        #key = request.form['key']
-        print(wordsU)
-
-    return jsonify({'prediccion': wordsU})
+        # key = request.form['key']
+        dataFrame1 = utils.load_from_csv('./in/dataAnimals.csv')
+        dataFrame2 = utils.load_from_csv('./in/dataDataScience.csv')
+        dataFrame3 = utils.load_from_csv('./in/dataEducation.csv')
+        dataFrame4 = utils.load_from_csv('./in/dataFashion.csv')
+        dataFrame5 = utils.load_from_csv('./in/dataPolitics.csv')
+        frames = [dataFrame1, dataFrame2, dataFrame3, dataFrame4, dataFrame5]
+        alldata = pd.concat(frames)
+        alldata_data = alldata.values[:, 2]
+        alldata_one = alldata.values[:, 1]
+        alldata_one = alldata_one[0]
+        print(alldata_one)
+        vectorizer = TfidfVectorizer()
+        x_train = vectorizer.fit_transform(alldata_data)
+        x_test = vectorizer.transform([wordsU])
+        prediction = model.predict(x_test)
+        return jsonify({'prediccion': list(prediction)})
 
 
 # TODO Sebestian Cristian Jimmy ampliar los request para un post con mensajes
